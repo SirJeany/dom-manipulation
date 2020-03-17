9f1e150aef1496635b37cfd47dc23e7c42b13dab
SirJeany <jmvanriet1@gmail.com>
2020-03-17 11:21:07 +0200

    Implemented the assignment

--- js/main.js
+++ js/main.js
@@ -1,20 +1,38 @@
 'use strict';
 
-const header = document.getElementById("main-heading");
-const aside = document.getElementById("aside");
+// const header = document.getElementById("main-heading");
+// const aside = document.getElementById("aside");
 
 
-header.innerHTML = "<h2> This is another heading </h2>";
-header.classList.add('text-right');
+// header.innerHTML = "<h2> This is another heading </h2>";
+// header.classList.add('text-right');
 
-// const heading = document.getElementsByTagName("h2");
+// // const heading = document.getElementsByTagName("h2");
 
-//remove:
-aside.classList.remove('bg-warning');
+// //remove:
+// aside.classList.remove('bg-warning');
 
-// Change the text allignement for each aside heading:
-const aside_headings = document.getElementsByClassName("aside-text");
+// // Change the text allignement for each aside heading:
+// const aside_headings = document.getElementsByClassName("aside-text");
 
-aside_headings.forEach(element => {
-    element.classList.add("text-right");
+// aside_headings.forEach(element => {
+//     element.classList.add("text-right");
+// });
+
+
+const p = document.getElementById("paragraph");
+const redBtn = document.getElementById("redBtn");
+const blueBtn = document.getElementById("blueBtn");
+
+
+redBtn.addEventListener("click", function() {
+    p.classList.remove('text-primary');
+    p.classList.add('red-text');
+    // alert('Adding red text');
+});
+
+blueBtn.addEventListener("click", function() {
+    p.classList.remove('red-text')
+    p.classList.add('text-primary');
+    // alert('Adding red text');
 });
