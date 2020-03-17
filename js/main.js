75cd7549d569a99d2d20cf6d85189ab04ca5bf4b
SirJeany <jmvanriet1@gmail.com>
2020-03-17 14:04:15 +0200

    Added an element using js

--- js/main.js
+++ js/main.js
@@ -19,10 +19,11 @@
 //     element.classList.add("text-right");
 // });
 
-
+const pSection = document.getElementById("pSection");
 const p = document.getElementById("paragraph");
 const redBtn = document.getElementById("redBtn");
 const blueBtn = document.getElementById("blueBtn");
+const changeBtn = document.getElementById("changeBtn");
 
 
 redBtn.addEventListener("click", function() {
@@ -32,7 +33,38 @@ redBtn.addEventListener("click", function() {
 });
 
 blueBtn.addEventListener("click", function() {
-    p.classList.remove('red-text')
+    p.classList.remove('red-text');
     p.classList.add('text-primary');
     // alert('Adding red text');
 });
+
+// Change the content of the paragraph 
+changeBtn.addEventListener("click", function() {
+    // pSection.innerHTML = "<p>This is the changed text</p>";
+    p.textContent = "This is the changed text";
+});
+
+
+// Add a new element:
+let newArticle = document.createElement('article');
+let newHeading = document.createElement('h2');
+let newParagraph = document.createElement('p');
+
+// Create content:
+newHeading.textContent = 'Article 001';
+newParagraph.textContent = 'lorum';
+
+// Stitch them together:
+newArticle.appendChild(newHeading);
+newArticle.appendChild(newParagraph);
+
+// Add some styling:
+newArticle.classList.add('bg-light', 'p-4', 'border');
+
+// Add atributes like id, etc
+newArticle.setAttribute('id', 'article001');
+
+// Add newArticle to page:
+// const newSection = document.getElementById("newSection");
+const main = document.querySelector('main');
+main.appendChild(newArticle);
